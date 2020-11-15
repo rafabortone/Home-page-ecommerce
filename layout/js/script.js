a9ab2ef042649dc1412d93f34395bc11763f5fa8
rafael <rafael@bluefoot.com.br>
2020-11-15 17:53:25 -0300

    slider banner desktop e mobile

--- layout/js/script.js
+++ layout/js/script.js
@@ -7,12 +7,39 @@ $('.header__icons--search, .header__autocomplete').click(function () {
   $('.header__autocomplete').toggleClass('header__autocomplete--active');
 })
 
+
+$('.header__menu--wrapper, .header__autocomplete--inputs').click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+})
+
+//footer
 $('.footer__menu--title').click(function () {
   const $this = $(this);
   $this.next($('.footer__menu--links')).toggleClass('footer__menu--links--active')
 })
 
-$('.header__menu--wrapper, .header__autocomplete--inputs').click(function (e) {
-  e.preventDefault();
-  e.stopPropagation();
+
+//banner
+$(document).ready(function () {
+  
+  const sliderPrincipal = $('#home__banner--slider')
+  if (sliderPrincipal.length) {
+    sliderPrincipal.slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: true,
+      arrows: true,
+    })
+  }
+    
+  const sliderPrincipalMobile = $('#home__banner--slider--mobile')
+  if (sliderPrincipalMobile.length) {
+    console.log('oppaaa');
+    sliderPrincipalMobile.slick({
+      autoplay: false,
+      dots: true,
+      arrows: true,
+    })
+  }
 })
