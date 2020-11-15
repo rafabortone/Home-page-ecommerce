ebc204d30f7f9168464f19780628e8cb358dda67
rafael <rafael@bluefoot.com.br>
2020-11-15 14:07:23 -0300

    ajusta busca autocomplete mobile

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,9 +1,14 @@
 //header
-$('.header__iconmenu, .header__menu').click(function (e) {
+$('.header__iconmenu, .header__menu').click(function () {
   $('.header__menu').toggleClass('header__menu--active');
 })
 
-$('.header__menu--wrapper').click(function (e) {
+$('.header__icons--search, .header__autocomplete').click(function () {
+  $('.header__autocomplete').toggleClass('header__autocomplete--active');
+})
+
+
+$('.header__menu--wrapper, .header__autocomplete--inputs').click(function (e) {
   e.preventDefault();
   e.stopPropagation();
 })
