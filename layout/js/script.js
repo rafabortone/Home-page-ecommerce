5988d7d4dda586b12a875b6c9b0417101f4a238a
rafael <rafael@bluefoot.com.br>
2020-11-15 21:36:41 -0300

    tipbar e marcas parceiras

--- layout/js/script.js
+++ layout/js/script.js
@@ -41,6 +41,92 @@ $(document).ready(function () {
     })
   }
 
+  const sliderTipBar = $('#tipbar-list')
+  if (sliderTipBar.length) {
+    sliderTipBar.slick({
+      autoplay: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      variableWidth: true,
+      responsive: [{
+          breakpoint: 1599,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+
+        },
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          },
+        },
+      ]
+    })
+  }
+
+  const sliderMarcas = $('#marcas-list')
+  if (sliderMarcas.length) {
+    sliderMarcas.slick({
+      autoplay: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      variableWidth: true,
+      responsive: [{
+          breakpoint: 1599,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+
+        },
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          },
+        },
+      ]
+    })
+  }
+
   const sliderShelfHome = $('#shelf-list')
   if (sliderShelfHome.length) {
     sliderShelfHome.slick({
